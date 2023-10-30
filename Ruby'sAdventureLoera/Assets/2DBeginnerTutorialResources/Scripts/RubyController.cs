904f449f006f51c0052aedcbfcce07afbaab1ea5
Lovantcom <146034448+Lovantcom@users.noreply.github.com>
2023-10-30 15:27:35 -0700

    Created Ruby
    Got all the assest for my Ruby project.

---
+++ Ruby'sAdventureLoera/Assets/2DBeginnerTutorialResources/Scripts/RubyController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 postition = transform.position;
+        postition.x = Position.x + 0.1f;
+        transform.position = postition;
+    }
+}
